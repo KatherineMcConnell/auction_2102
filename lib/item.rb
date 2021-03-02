9f97ae90bedb46ee96087cd4b9bff57fccb06f29
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-03-02 14:58:42 -0700

    Assert;Pass item.bid

--- lib/item.rb
+++ lib/item.rb
@@ -3,5 +3,14 @@ class Item
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def bids
+    @bids
+  end
+
+  def add_bid(addendee_num, budget)
+    @bids << [:attendee_num] = budget
   end
 end
