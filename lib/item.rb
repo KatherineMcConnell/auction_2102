ee26af64595ad9d4a2b41d3007cab3d5e78185cf
Katherine McConnell <mcconnell.katherine01@gmail.com>
2021-06-10 14:41:41 -0600

    Update item.rb

--- lib/item.rb
+++ lib/item.rb
@@ -1,16 +1,1 @@
-class Item
-  attr_reader :name
 
-  def initialize(name)
-    @name = name
-    @bids = {}
-  end
-
-  def bids
-    @bids
-  end
-
-  def add_bid(addendee, budget)
-    @bids[:attendee] = budget
-  end
-end
