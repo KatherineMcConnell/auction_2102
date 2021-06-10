1ed2d48bdac96cbf589cd07c2957c1f6c37101ec
Katherine McConnell <mcconnell.katherine01@gmail.com>
2021-06-10 14:40:56 -0600

    Update attendee.rb

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,13 +1,1 @@
-class Attendee
-  attr_reader :name
 
-  def initialize(args)
-    @name = args[:name]
-    @budget = args[:budget]
-    end
-
-  def budget
-    int_budget = @budget.delete'$'
-    int_budget.to_i
-  end
-end
